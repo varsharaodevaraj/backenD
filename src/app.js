82aba1923e4d3ca1bb969ad5d26fddea3189d301
Varsha Rao <varsharaodevaraj@gmail.com>
2025-08-04 14:43:27 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,29 +1,28 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-const app = express()
+const app = express();
 
-// app.use(cors())
 app.use(cors({
     origin: process.env.CORS_ORI,
     credentials: true
-}))
+}));
+
 app.use(express.json({
     limit: "16kb"
-}))
+}));
 app.use(express.urlencoded({
     extended: true,
     limit: "16kb"
-}))
-app.use(express.static("public"))
-app.use(cookieParser())
+}));
 
-// import routes
-import userRoute from './routes/user.routes.js'
+app.use(express.static("public"));
+app.use(cookieParser());
 
-// routes declaration:
-app.use("/api/v1/users",userRoute)
+import userRoute from './routes/user.routes.js';
 
 
-export default app
+app.use("/api/v1/users", userRoute);
+
+export default app;
